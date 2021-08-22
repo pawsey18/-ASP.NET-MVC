986229aaec5eb94a9daa7d954584dc4af6a56a09
Nick Pawsey <pawsey18@gmail.com>
2021-08-22 09:19:54 -0300

    Following Jamie Munro's step by step book of trash
    This book is an example of what not to do with MVC.

---
+++ BSDemo/BSDemo/Migrations/202108212242034_anotherone.cs
@@ -0,0 +1,16 @@
+namespace BSDemo.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class anotherone : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
